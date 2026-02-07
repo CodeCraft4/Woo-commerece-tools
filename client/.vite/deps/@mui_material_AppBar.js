3b8ca43a0cf4a318e72472683f3b3446269622b6
Imad Shah <shahimad499@gmail.com>
2026-02-07 19:53:37 +0500

    fix in the product popup

---
+++ client/.vite/deps/@mui_material_AppBar.js
@@ -0,0 +1,25 @@
+import {
+  AppBar_default,
+  appBarClasses_default,
+  getAppBarUtilityClass
+} from "./chunk-UNEKM2NO.js";
+import "./chunk-BKOVUZUP.js";
+import "./chunk-G4FQZWC4.js";
+import "./chunk-JX37XSKT.js";
+import "./chunk-NKEIIA4J.js";
+import "./chunk-QTKHT3CG.js";
+import "./chunk-SV3CTAPR.js";
+import "./chunk-4VSWXUFW.js";
+import "./chunk-3SET6WWK.js";
+import "./chunk-LNJWJNFR.js";
+import "./chunk-DLPUH77Q.js";
+import "./chunk-XP4JWPZJ.js";
+import "./chunk-6BGGTDXO.js";
+import "./chunk-N5SUCCGT.js";
+import "./chunk-6VIQC2ON.js";
+import "./chunk-HXA6O6EE.js";
+export {
+  appBarClasses_default as appBarClasses,
+  AppBar_default as default,
+  getAppBarUtilityClass
+};
---
+++ client/.vite/deps/chunk-BH4YWFKC.js
@@ -0,0 +1,20 @@
+import {
+  createSvgIcon
+} from "./chunk-AQQN7S6F.js";
+import {
+  require_jsx_runtime
+} from "./chunk-XP4JWPZJ.js";
+import {
+  __toESM
+} from "./chunk-HXA6O6EE.js";
+
+// node_modules/@mui/icons-material/esm/Menu.js
+var import_jsx_runtime = __toESM(require_jsx_runtime());
+var Menu_default = createSvgIcon((0, import_jsx_runtime.jsx)("path", {
+  d: "M3 18h18v-2H3zm0-5h18v-2H3zm0-7v2h18V6z"
+}), "Menu");
+
+export {
+  Menu_default
+};
+//# sourceMappingURL=chunk-BH4YWFKC.js.map
